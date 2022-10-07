2fde9b02c15d8e28e5c9e502a7ea5280bd0db992
San <lahpaimufc@gmail.com>
2022-10-06 20:54:17 -0500

    hw3

---
+++ app/src/main/java/com/example/signin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+   //     setContentView(R.layout.activity_main)
+        setContentView(R.layout.problem2_categories)
+   //     setContentView(R.layout.problem2_createacc )
+    //    setContentView(R.layout.problem2_calculator)
+    //    setContentView(R.layout.problem1_singin_constraint)
+    }
+}
