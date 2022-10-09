64e65b28c522acc8050dd859fd3f7e22765ea954
Lahpai <47212045+Lahpai@users.noreply.github.com>
2022-10-09 18:21:52 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/signin/MainActivity.kt
+++ app/src/main/java/com/example/signin/MainActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-   //     setContentView(R.layout.activity_main)
-        setContentView(R.layout.problem2_categories)
+        setContentView(R.layout.activity_main)
+   //     setContentView(R.layout.problem2_categories)
    //     setContentView(R.layout.problem2_createacc )
     //    setContentView(R.layout.problem2_calculator)
     //    setContentView(R.layout.problem1_singin_constraint)
